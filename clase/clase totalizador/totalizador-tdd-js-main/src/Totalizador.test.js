27809dbe8e92bf6814c0ce396a9e3c11d8654c1d
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-04 16:38:48 -0400

    deberia calcular para una cantidad cualquiera

--- clase/clase totalizador/totalizador-tdd-js-main/src/Totalizador.test.js
+++ clase/clase totalizador/totalizador-tdd-js-main/src/Totalizador.test.js
@@ -2,8 +2,11 @@ describe("Totalizador ", () => {
   it("deberia calcular para una cantidad por defecto", () => {
     expect(calcularTotal(2, 1, "")).toEqual(2);
   });
+  it("deberia calcular para una cantidad calquiera", () => {
+    expect(calcularTotal(2, 1, "")).toEqual(2);
+  });
 });
 
 function calcularTotal(cantidad, precio, estado) {
-  return 2;
+  return cantidad * precio;
 }
