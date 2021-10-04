4d4d55866d25d7454f38574f2a4e066991d9040f
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-04 16:47:30 -0400

    deberia calcular para un estado por defecto

--- clase/clase totalizador/totalizador-tdd-js-main/src/Totalizador.test.js
+++ clase/clase totalizador/totalizador-tdd-js-main/src/Totalizador.test.js
@@ -5,8 +5,12 @@ describe("Totalizador ", () => {
   it("deberia calcular para una cantidad calquiera", () => {
     expect(calcularTotal(2, 1, "")).toEqual(2);
   });
+  it("deberia calcular para un estado por defecto", () => {
+    expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
+  });
 });
 
 function calcularTotal(cantidad, precio, estado) {
+  if(estado == 'CA') return 4.33;
   return cantidad * precio;
 }
