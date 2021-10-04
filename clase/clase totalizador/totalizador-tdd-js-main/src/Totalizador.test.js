654b324d863737fd35e834e054da5e16d82cfb0b
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-10-04 16:56:45 -0400

    deberia calcular para el estado de NV

--- clase/clase totalizador/totalizador-tdd-js-main/src/Totalizador.test.js
+++ clase/clase totalizador/totalizador-tdd-js-main/src/Totalizador.test.js
@@ -11,10 +11,13 @@ describe("Totalizador ", () => {
   it("deberia calcular para el estado de UT", () => {
     expect(calcularTotal(2, 2, "UT")).toEqual(4.266);
   });
+  it("deberia calcular para el estado de NV", () => {
+    expect(calcularTotal(2, 2, "NV")).toEqual(4.32);
+  });
 });
 
 function impuestoEstado(estado){
-  let impuestos = { CA: 0.0825, UT: 0.0665, "": 0 };
+  let impuestos = { CA: 0.0825, UT: 0.0665, NV: 0.08, "": 0 };
   return impuestos[estado];
 }
 
